6cefa3c9b094a4287655ac4c2f84c765ebc2d481
Amanda <airlinepilotamanda@gmail.com>
2026-09-20 10:27:39 +0000

    Add Netlify request path diagnostics

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,10 @@
 const serverless = require("serverless-http");
+
 const app = require("../../server");
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, {
+  request: (request) => {
+    console.log("Netlify request path:", request.path);
+    console.log("Netlify request URL:", request.url);
+  }
+});
