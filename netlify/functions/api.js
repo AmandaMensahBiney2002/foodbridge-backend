5333a1b4160fdecd698b277e2a0ac0d582945109
Amanda <airlinepilotamanda@gmail.com>
2026-09-20 15:31:31 +0000

    Update Netlify configuration

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,9 +2,4 @@ const serverless = require("serverless-http");
 
 const app = require("../../server");
 
-module.exports.handler = serverless(app, {
-  request: (request) => {
-    console.log("Netlify request path:", request.path);
-    console.log("Netlify request URL:", request.url);
-  }
-});
+module.exports.handler = serverless(app);
